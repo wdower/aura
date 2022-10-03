0d1c724c5f9dc0b6eca9c44b22d424fe9afa8342
Christopher Hinchey <chinchey@mitre.org>
2022-10-03 17:03:44 -0400

    Updated footer layout and logos
    Signed-off-by: Christopher Hinchey <chinchey@mitre.org>

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -3,68 +3,8 @@
     height="auto"
     class="ma-0"
     :class="this.$vuetify.theme.dark == true ? 'grey darken-4' : 'white'"
+    style="padding: 6px 16px;"
   >
-    <!-- style="border-color:#e0e0e0;border-width: 1px;border-style: solid;border-bottom: 0;border-left:0;border-right:0" -->
-    <!--<v-container fluid>
-      <v-row align="center" justify="center" class="my-0 py-0">
-        <v-col cols="12" md="10" lg="10" sm="10" class="py-0 my-0">
-          <v-divider v-if="!this.$vuetify.theme.dark"></v-divider>
-          <p class="google-font mt-4 mb-3" style="font-size:130%">
-            Follow Us:
-            <span v-for="(item,i) in communityData.SocialLinks" :key="i">
-              <v-tooltip top>
-                <template v-slot:activator="{ on }">
-                  <v-btn text icon :href="item.link" target="_blank" slot="activator">
-                    <v-icon>{{item.icon}}</v-icon>
-                  </v-btn>
-                </template>
-                <span>{{item.name}}</span>
-              </v-tooltip>
-            </span>
-          </p> 
-
-        </v-col>
-    </v-row>-->
-
-    <!--<v-row align="center" justify="center">
-        <v-col cols="12" md="10" lg="10" sm="10" class="my-0 py-0">
-          <v-container fluid class="pa-0 my-0">
-
-            <v-row align="center" justify="start" class>
-              <v-col
-                cols="12"
-                md="3"
-                lg="4"
-                sm="3"
-                class="py-2"
-                v-for="(item,i) in FooterData.Links"
-                :key="i"
-              >
-               <div v-for="(itemp,j) in item" :key="j" class="mt-1">
-                  <p class="google-font mb-0" style="font-size: 140%;font-weight: 300;">
-                    <b>{{j}}</b>
-                  </p>
-                  <div v-for="(linkdata,k) in itemp" :key="k">
-                    <a
-                      :href="linkdata.Link"
-                      :class="$vuetify.theme.dark?'darkThemeTextLink':'lightThemeTextLink'"
-                      class="google-font"
-                      target="_blank"
-                      style="text-decoration: none;font-size:110%"
-                    >{{linkdata.LinkName}}</a>
-                    <br />
-                  </div>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-col>
-    </v-row>-->
-
-    <v-container>
-      <v-row align="center" justify="center" class="mb-0">
-        <v-col md="12" lg="10" xs="12" class="px-0 mx-0">
-          <v-divider></v-divider>
           <v-toolbar
             text
             class="pa-0 px-0 mt-3 mx-0 elevation-0"
@@ -73,15 +13,7 @@
               this.$vuetify.theme.dark == true ? 'grey darken-4' : 'white'
             "
           >
-          <a href="https://www.netlify.com"> 
-            <img
-              :src="require('@/assets/img/svg/netlify' + imgVersion + '.svg')"
-              style="width: 6rem; height: 6rem"
-              class="mt-1"
-            />
-            </a>
-            <v-spacer />
-            <p
+          <p
               class="google-font pl-0 ml-0"
               :style="
                 this.$vuetify.breakpoint.smAndDown ? '' : 'font-size:200%'
@@ -94,6 +26,17 @@
               />
               MITRE SAF
             </p>
+          
+            <v-spacer />
+            <a href="https://www.netlify.com"> 
+              <img
+                :src="require('@/assets/img/svg/netlify' + imgVersion + '.svg')"
+                style="width: 2.5rem; height: 2.5rem; vertical-align: middle"
+                class="mr-5"
+              />
+              </a>
+              <v-icon v-if="this.$vuetify.theme.dark == true" href="https://github.com/mitre/mitre-saf" x-large color="white">mdi-github</v-icon>
+              <v-icon v-if="this.$vuetify.theme.dark == false" href="https://github.com/mitre/mitre-saf" x-large color="black">mdi-github</v-icon>
             <v-spacer />
             <p
               class="google-font pl-0"
@@ -101,21 +44,7 @@
             >
               The MITRE Corporation © {{ new Date().getFullYear() }}
             </p>
-
-            <!-- <v-btn
-              v-for="(item,i) in FooterData.FooterEndSession"
-              :key="i"
-              :href="item.Link"
-              target="_blank"
-              class="ml-0 google-font hidden-sm-and-down"
-              style="text-transform: capitalize;font-size:110%"
-              text
-              aria-label="Footer Bottom Button"
-            >{{ item.LinkName }}</v-btn>-->
           </v-toolbar>
-        </v-col>
-      </v-row>
-    </v-container>
   </v-footer>
 </template>
 
